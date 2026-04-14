b6223f16e1089b3a88bc59f2bea234ab8cc7a590
Sergio Alejandro Medrano Díaz <sergioamdiaz@hotmail.com>
2026-04-14 16:47:38 -0400

    Update with changes in notebook 02_training_dae

--- src/data.py
+++ src/data.py
@@ -17,7 +17,7 @@ logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 #-------------------------------------------------------------------------------
 
 SEED = 42
-IMG_SIZE = (512, 512)
+IMG_SIZE = (576, 448)
 BATCH_SIZE = 8
 AUTOTUNE = tf.data.AUTOTUNE # -1
 
@@ -95,10 +95,10 @@ def split_train_val(img_paths : list[Path], mask_paths : list[Path], val_split: 
 # Load Image and Mask:
 
 def _load_image_tf(tf_image_path: tf.Tensor) -> tf.Tensor:
-    ''' 
-    Loads an image from the given path and preprocesses it for training. 
+    '''
+    Loads an image from the given path and preprocesses it for training.
     Only supports JPG images.
-    Returns a tensor of shape (H, W, 3) and dtype float32 with pixel values in [0,1]. 
+    Returns a tensor of shape (H, W, 3) and dtype float32 with pixel values in [0,1].
     '''
     image_bytes = tf.io.read_file(tf_image_path) # read the image file as a bytes string
     image = tf.image.decode_jpeg(image_bytes, channels=3) # decode the bytes string into a tensor of shape (H, W, 3) and dtype uint8
@@ -107,14 +107,14 @@ def _load_image_tf(tf_image_path: tf.Tensor) -> tf.Tensor:
     return image
 
 def _load_mask_tf(tf_mask_path: tf.Tensor) -> tf.Tensor:
-    ''' 
-    Loads a mask from the given path and preprocesses it for training (loss function). 
-    Only supports PNG images. 
-    Returns a tensor of shape (H, W, 1) and dtype float32 with pixel values in [0,1]. 
+    '''
+    Loads a mask from the given path and preprocesses it for training (loss function).
+    Only supports PNG images.
+    Returns a tensor of shape (H, W, 1) and dtype float32 with pixel values in [0,1].
     '''
     mask_bytes = tf.io.read_file(tf_mask_path) # read the image file as a bytes string
     mask = tf.image.decode_png(mask_bytes, channels=1) # decode the bytes string into a tensor of shape (H, W, 1) and dtype uint8
-    mask = tf.image.convert_image_dtype(mask, tf.float32)  # Rescale to [0,1] (consistency with the rest of the pipeline) 
+    mask = tf.image.convert_image_dtype(mask, tf.float32)  # Rescale to [0,1] (consistency with the rest of the pipeline)
     tf.debugging.assert_equal(tf.shape(mask)[:2], IMG_SIZE) # Debugging: Double check the image size
     return mask
 
@@ -139,11 +139,11 @@ def _add_mild_corruption(image: tf.Tensor, corruption_factor: float) -> tf.Tenso
 
     return x
 
-def corrupt_clean_mask_from_path(tf_image_path: tf.Tensor, 
+def corrupt_clean_mask_from_path(tf_image_path: tf.Tensor,
                                  tf_mask_path: tf.Tensor,
                                  corruption_factor: float,
-                                 ) -> tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
-    ''' 
+                                 ) -> tuple[tf.Tensor, tf.Tensor]:
+    '''
     Loads the image at the given path, creates a mildly corrupted version of it.
     Loads the mask at the given path.
     Returns the image, currupted image, and mask as tensors.
@@ -151,7 +151,10 @@ def corrupt_clean_mask_from_path(tf_image_path: tf.Tensor,
     clean = _load_image_tf(tf_image_path)
     corrupted = _add_mild_corruption(clean, corruption_factor)
     mask = _load_mask_tf(tf_mask_path)
-    return corrupted, clean, mask
+
+    y_true = tf.concat([clean, mask], axis=-1)  # Concat into tensor so it matches with what model.fit() expects. (H, W, c)
+
+    return corrupted, y_true
 
 #-------------------------------------------------------------------------------
 # Create Dataset:
@@ -161,12 +164,12 @@ def create_dataset(image_paths: list[Path],
                    shuffle: bool = True,
                    corruption_factor: float = 0.8) -> tf.data.Dataset:
     '''
-    Creates a dataset containing triples of the form (corrupted_image, clean_image, mask). 
+    Creates a dataset containing triples of the form (corrupted_image, clean_image, mask).
     '''
     if len(image_paths) != len(mask_paths):
         raise ValueError(f"image_paths and mask_paths must have the same length, "
                          f"got {len(image_paths)} and {len(mask_paths)}.")
-        
+
     # Create a dataset with the list of tuples of paths (elements will be tf.Tensors tuples):
     lazy_ds = tf.data.Dataset.from_tensor_slices(
         ([str(p) for p in image_paths], [str(p) for p in mask_paths]) # tuples
@@ -174,10 +177,10 @@ def create_dataset(image_paths: list[Path],
     # Shuffles (the paths tuples) to ensure random order at each epoch during training:
     lazy_ds = (lazy_ds.shuffle(buffer_size=len(image_paths), seed=SEED)) if shuffle else lazy_ds
     # Apply corruption and loading in parallel to speed up the data pipeline:
-    lazy_ds = lazy_ds.map(lambda tf_image_path, tf_mask_path: 
+    lazy_ds = lazy_ds.map(lambda tf_image_path, tf_mask_path:
         corrupt_clean_mask_from_path(tf_image_path, tf_mask_path, corruption_factor),
-        num_parallel_calls=AUTOTUNE) 
-    # Each element of the dataset is now a triple (corrupted_image, clean_image, mask)
+        num_parallel_calls=AUTOTUNE)
+    # Each element of the dataset is now a tensor tuple: (corrupted_image, packed_clean_mask)
 
     # Batch the dataset:
     lazy_ds = lazy_ds.batch(BATCH_SIZE)
